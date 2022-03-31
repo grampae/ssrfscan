e6afec6bfcc66e3fcdcfcb027a0d5ba45ca73f46
Grampa-E <36344197+grampae@users.noreply.github.com>
2022-03-31 08:30:32 -0400

    Add files via upload

--- ssrfscan.py
+++ ssrfscan.py
@@ -12,13 +12,14 @@ import base64
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 parser = argparse.ArgumentParser(description="ssrf portscan")
+parser.add_argument("-s", dest="ssrfurl", required=True, help="SSRF Url")
+parser.add_argument("-c", dest="scheme", required=False, help="Define scheme, such as http://, netdoc:///, gopher://, defaults to https://", default="https://")
+parser.add_argument("-t", dest="target", required=False, help="Target to scan, such as 127.0.0.1", default="127.0.0.1")
 parser.add_argument("-p", dest="ports", required=False, help="Comma seperated ports to scan", default="21,22,23,25,53,80,110,111,135,139,143,443,445,993,995,1723,3306,3389,5900,8080")
 parser.add_argument("-u", dest="path", required=False, help="Path to request, defaults to /favicon.ico", default="/favicon.ico")
-parser.add_argument("-t", dest="target", required=False, help="Target to scan, such as 127.0.0.1", default="127.0.0.1")
-parser.add_argument("-b", dest="prox", required=False, help="Proxy through burp", action="store_true")
-parser.add_argument("-c", dest="scheme", required=False, help="Define scheme, such as http://, netdoc:///, gopher://, defaults to https://", default="https://")
 parser.add_argument("-e", dest="enc", required=False, help="Encode payload (choose b64 or url)", default="")
-parser.add_argument("-s", dest="ssrfurl", required=True, help="SSRF Url")
+parser.add_argument("-b", dest="prox", required=False, help="Proxy through burp", action="store_true")
+
 args = parser.parse_args()
 port1 = args.ports
 path = args.path
@@ -34,7 +35,6 @@ def handler(signum, frame):
     res = input(" Ctrl-c was pressed. Do you really want to exit? y/n ")
     if res == 'y':
         exit(1)
-
 signal.signal(signal.SIGINT, handler)
 if prox == True:
     proxies = { 'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080' }
